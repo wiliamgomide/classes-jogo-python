8faff4eff7ec50897107b4940d8523c9c97f5aaf
wiliamgomide <wiliampintogomide@gmail.com>
2025-08-02 09:33:54 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from heroi import Heroi
+
+heroi1 = Heroi("Aragorn", 35, "guerreiro")
+heroi2 = Heroi("Gandalf", 99, "mago")
+heroi3 = Heroi("Bruce", 29, "monge")
+heroi4 = Heroi("Hanzo", 27, "ninja")
+
+heroi1.atacar()
+heroi2.atacar()
+heroi3.atacar()
+heroi4.atacar()
